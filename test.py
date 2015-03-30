10a869c3c55a83ea479ac91f5db72ae92ce04a42
Erkan Narmanli <qwann@hotmail.fr>
2015-03-30 16:07:23 +0200

    message

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 
 from libmandel import *
 
-taille = 10000 
+taille = 1000 
 
 image = cree_mandelbrot_zoli(taille = taille, couleur_fond = (20,20,20) , couleur_bord = (220, 220, 220), couleur_mandel = (5,5,5))
 
