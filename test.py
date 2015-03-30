43311d5d88d22b7085b64d11dde0070e4567946d
Erkan Narmanli <qwann@hotmail.fr>
2015-03-30 15:03:54 +0200

    C'est beau ! :)

--- test.py
+++ test.py
@@ -3,5 +3,10 @@
 
 from libmandel import *
 
-image = cree_mandelbrot_zoli(taille = 4000)
+taille = 1000 
+
+image = cree_mandelbrot_zoli(taille = taille, couleur_fond = (16,27,37) , couleur_bord = (94, 40, 0))
+image = ameliore_bord(image, taille)
 image.show()
+
+enregistre(image, 'Mandel_zoli')
