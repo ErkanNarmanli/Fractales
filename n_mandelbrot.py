66bf12e64455ac4764cd0806410842c24a5e2176
Martin Pépin <martin.pepin@netcourrier.com>
2015-04-05 11:27:02 +0200

    MàJ de n_mandelbrot.py

--- n_mandelbrot.py
+++ n_mandelbrot.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 # -*-coding:utf-8 -*
 
+"""
+Dessine une généralisation de l'ensemble de Mandelbrot
+"""
+
+from libfractales import *
 from PIL import Image, ImageDraw, ImageFont
 import sys
 from cmath import *
@@ -30,48 +35,6 @@ image = Image.new('RGB', (taille, taille), (255, 255, 255))
 # Outil de dessin
 draw = ImageDraw.Draw(image)
 
-# Fonction qui aligne du texte à droite dans un espace de t_max caractères
-def ecrire(chaine,t_max):
-	for m in range(t_max-len(chaine)):
-		sys.stdout.write(" ")
-	sys.stdout.write(chaine)
-
-# Affiche une barre de chargement
-def chargement(k):
-	pourc = 100*k/float(taille)
-	pourv = 20*pourc/100.  #pourvingt, parce que j'ai envie
-	pourc = int(pourc)+1
-	pourv = int(pourv)+1
-	sys.stdout.write("\r")
-	ecrire("{}%:".format(pourc),5)
-	ecrire(str(k+1),len(str(taille)))
-	sys.stdout.write("/{} pix".format(taille))
-	sys.stdout.write("   [")
-	for m in range(pourv):
-		sys.stdout.write(":")
-	for m in range(20-pourv):
-		sys.stdout.write(" ")
-	sys.stdout.write("]")
-	sys.stdout.flush()
-	return
-
-# Détermine les coordonnées dans le plan complexe
-# à partir des coordonnées dans l'image
-def ch_coord(k, l):
-	return(complex( \
-		(float(k) - taille/2.)*largeur/float(taille) + centre, \
-		(float(l) - taille/2.)*largeur/float(taille) \
-		))
-
-# Détermine la couleur du pixel en fonction du rang à partir
-# duquel on sort du cercle de rayon 2
-def couleur_pix(n):
-	if n == n_max:
-		c = 0
-	else:
-		c = 20 + 235*((n_max - n)/float(n_max))**alpha
-		c = int(c)
-	return(c, c, c)
 
 # Parcours
 for k in range(taille):
