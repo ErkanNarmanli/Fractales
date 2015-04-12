1cba5020307f0661c584d0d7aa64df4a26837bf4
Martin Pépin <martin.pepin@netcourrier.com>
2015-04-12 10:05:34 +0200

    C'est mieux mais on n'arrive pas à join

--- brocieliande.py
+++ brocieliande.py
@@ -4,24 +4,26 @@
 from libjulia import *
 from multiprocessing import *
 import time
-import sys
 
 t = time.time()
 
-def worker(a,b):
-	for i in range(a,b):
-		chargement(i,100)
-		image = cree_julia(taille = 100, c = complex(0,-i*(0.001)), verbose = False)
-	return
+def worker(a, b, q):
+	for i in range(a, b):
+		q.put(cree_julia(taille = 100, c = complex(0,i*0.01), verbose = False))
+	print('fini : {}'.format((a, b)))
 
 jobs = []
 
+images_q = Queue()
+
 for k in range(4):
-	p = Process(target = worker, args = (k*25,k*25+25))
+	p = Process(target = worker, args = (k*25, (k+1)*25, images_q))
 	jobs.append(p)
 	p.start()
-	p.join()
 
+for p in jobs:
+	p.join()
+	print('Joined {}'.format(p))
 
 t_1 = time.time() - t
 print("Temps d'execution 1 : {}".format(t_1))
@@ -31,7 +33,7 @@ t = time.time()
 
 for k in range(100):
 	chargement(k,100)
-	image = cree_julia(taille = 100, c = complex(0,-k*(0.001)), verbose = False)
+	image = cree_julia(taille = 100, c = complex(0,k*(0.01)), verbose = False)
 	
 t_2 = time.time() - t
 
