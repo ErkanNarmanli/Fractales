02ae297cd5fe997170fd182f3e99195f7bd634ce
Martin Pépin <martin.pepin@netcourrier.com>
2015-04-12 07:54:38 +0200

    xrange au lieu de range

--- libjulia.py
+++ libjulia.py
@@ -8,9 +8,11 @@ Fonctions dessinant des ensembles de Julia
 from libfractales import *
 from PIL import Image, ImageDraw
 from math import sqrt
+from multiprocessing import Pool
 import numpy as np
 import time
 import sys
+
 
 
 def cree_julia(taille=600, c = 0, n_max = 200, alpha = 5, largeur = 4.2, verbose = True):
@@ -22,18 +24,18 @@ def cree_julia(taille=600, c = 0, n_max = 200, alpha = 5, largeur = 4.2, verbose
 	image = Image.new('RGB', (taille, taille), (255, 255, 255))
 	# Outil de dessin
 	draw = ImageDraw.Draw(image)
-	for k in range(taille):
+	for k in xrange(taille):
 		if verbose:
 			chargement(k, taille)
-		for l in range(taille/2+1):
-			u = ch_coord(k,l,taille, largeur, 0)
+		for l in xrange(taille/2):
+			u = ch_coord(k, l, taille, largeur, 0)
 			n = 0
 			while abs(u) <= 2 and n < n_max:
 				u = u*u + c
 				n = n + 1
 			col = couleur_pix(n, n_max, alpha)
-			draw.point((k, l), fill=col)
-			draw.point((taille-k-1,taille-l-1), fill=col)
+			draw.point((k, l), fill = col)		
+			draw.point((taille - 1 - k, taille - 1 - l), fill = col)
 	if verbose:
 		print("  Image générée")
 		print("Temps d'exécution : {} s".format(time.time() - t))
